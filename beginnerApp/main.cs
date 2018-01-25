0bed67b62295a89728ecaa0dad3d5f98989d70ef
everett <everett.frisch@ibsupply.com>
2018-01-25 13:47:33 -0600

    sinished calc pi, added reverse string and detect palindrome

--- beginnerApp/main.cs
+++ beginnerApp/main.cs
@@ -25,6 +25,12 @@ class BeginningApp
                 case 1:
                     calcPiToNth();
                     break;
+                case 2:
+                    reverseString();
+                    break;
+                case 3:
+                    detectPalindrome();
+                    break;
                 default:
                     Console.WriteLine("Invalid choice!");
                     break;
@@ -34,7 +40,42 @@ class BeginningApp
 
     static void calcPiToNth()
     {
-        Console.WriteLine("****Calculating Pi to the Nth degree****\nPlease enter the value for n:");
+        Console.WriteLine("****Calculating Pi to the Nth degree****\nPlease enter a value, less no more than 15, for n:");
+        string input = Console.ReadLine();
+        int n;
+        if (!(int.TryParse(input, out n)) || n > 15 || n < 0) {
+            Console.WriteLine("ERROR: Input must be a positive integer no more than 15. Exiting Calc pi to nth degree...");
+            return;
+        }
+        Console.WriteLine(Math.Round(Math.PI, n));
+    }
+
+    static void reverseString()
+    {
+        Console.WriteLine("****Reverse a string****\nPlease enter a string to reverse:");
+        string input = Console.ReadLine();
+        char[] characterArray = input.ToCharArray();
+        string newString = "";
+        for (int i = characterArray.Length-1; i >= 0; i--) {
+            newString += characterArray[i];
+        }
+        Console.WriteLine(newString);
+    }
+
+    static void detectPalindrome()
+    {
+        Console.WriteLine("****Reverse a string****\nPlease enter a string to detect whether it is a palindrome or not:");
+        string input = Console.ReadLine();
+        char[] characterArray = input.ToCharArray();
+        int left = 0;
+        int right = characterArray.Length-1;
+        while (left <= right) {
+            if (characterArray[left++] != characterArray[right--]) {
+                Console.WriteLine("{0} is NOT a palindrome", input);
+                return;
+            }
+        }
+        Console.WriteLine("{0} is indeed a palindrome", input);
     }
 }
 
@@ -43,7 +84,9 @@ class Actions
     //always try to use string instead of String
     public string[] _actions = {
         "Exit App",
-        "Calculate Pi to the Nth degree"
+        "Calculate Pi to the Nth degree",
+        "Reverse a string",
+        "Detirmine a palindrome"
     };
 
     public string[] getActions()
